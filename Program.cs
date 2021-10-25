ccd6f76de060430c07e84745bd067b2cab7a83fb
JavHalla <73223185+Paulmcginley93@users.noreply.github.com>
2021-10-25 15:23:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿
+
+using System;
+
+namespace ConsoleApp3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stopWatch = new StopWatch();
+            bool isTrue = true;
+            string userInput;
+            bool isStartedTime = false;
+            bool isStopped = false;
+
+
+            do
+            {
+                
+                Console.WriteLine($"Enter Start to 'start' the stop watch");
+                Console.WriteLine($"Enter Stop to 'stop' the stop watch");
+                Console.WriteLine($"Enter Exit to 'exit' the stop watch");
+                userInput = Console.ReadLine();
+                Console.WriteLine();
+
+
+
+                if (userInput.ToLower() == "start")
+                {
+
+                    if (isStartedTime == false)
+                    {
+                        stopWatch.Start();
+                        Console.WriteLine("The stop watch has started \n");
+                        isStartedTime = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The timer has already started!  \n");
+
+                    }
+
+                }
+
+                else if (userInput.ToLower() == "stop")
+                {
+
+                    if (isStopped == false)
+                    {
+                        stopWatch.Stop();
+                        Console.WriteLine($"The stop watch has stopped and the timer is: [{stopWatch.GetInterval()}] \n");
+
+                        isStartedTime = false;
+
+                        isStopped = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Stop Watch has already been stopped! \n");
+                    }
+                }   
+                
+                else if (userInput.ToLower() == "exit")
+                {
+                    isTrue = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR: Please try again. You did not enter a valid input \n");
+
+                }
+                
+            }
+            while (isTrue);
+
+            Console.WriteLine("Thanks for using the the stopwatch");
+
+           
+
+           
+        }
+    }
+}
+
+
---
+++ StopWatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    public class StopWatch
+    {
+       private DateTime StartTime;
+       private DateTime FinishTime;
+      
+
+       
+
+        public void Start()
+        {
+            StartTime = DateTime.Now;
+            
+        }
+
+        public void Stop()
+        {
+           
+            FinishTime = DateTime.Now;
+            
+        }
+
+        public TimeSpan GetInterval()
+        {
+            return FinishTime - StartTime;
+      
+        }
+    }
+}
+
+
